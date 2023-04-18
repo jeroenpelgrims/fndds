ca47d77fc18df96b7939e08a817c0dce2cb6e4fe
Jeroen pelgrims <resurge@jeroen-laptop.jeroenpelgrims.com>
2023-04-18 23:55:51 +0200

    Add first working endpoint

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,10 +2,19 @@ import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-const users = sqliteTable("users", {
+export const nutrients = sqliteTable("nutrient", {
   id: integer("id").primaryKey(),
-  fullName: text("full_name"),
+  name: text("name").notNull(),
+  unitName: text("unit_name").notNull(),
+  nutrientNumber: integer("nutrient_nbr"),
+  rank: integer("rank"),
 });
 
-const sqlite = new Database("food.db");
-const db = drizzle(sqlite);
+export const food = sqliteTable("food", {
+  fdcId: integer("fdc_id").primaryKey(),
+  data_type: text("data_type").notNull(),
+  description: text("description").notNull(),
+});
+
+const sqlite = new Database("../food.db");
+export const db = drizzle(sqlite);
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,22 @@
 import { serve } from "@hono/node-server";
+import { like } from "drizzle-orm";
 import { Hono } from "hono";
+import { db, food } from "./db";
 
 const app = new Hono();
 
-app.get("/", async (c) => c.text("Hello Hono!"));
+app.get("/autocomplete", async (c) => {
+  const text = c.req.query("text") ?? "";
+  const results = await db
+    .select({
+      id: food.fdcId,
+      description: food.description,
+    })
+    .from(food)
+    .where(like(food.description, `%${text}%`))
+    .limit(20)
+    .all();
+  return c.json(results);
+});
 
 serve(app);
