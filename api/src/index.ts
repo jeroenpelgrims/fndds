3b561aed4d18459ff33b638eb0731439ae4f07ec
Jeroen pelgrims <resurge@jeroen-laptop.jeroenpelgrims.com>
2023-04-19 22:57:53 +0200

    Allow multiple words in autocomplete

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "@hono/node-server";
-import { like, sql } from "drizzle-orm";
+import { and, like, sql } from "drizzle-orm";
 import { Hono } from "hono";
 import { db, food } from "./db";
 
@@ -7,18 +7,41 @@ const app = new Hono();
 
 app.get("/autocomplete", async (c) => {
   const text = c.req.query("text") ?? "";
+  const texts = text
+    .replace(/,/g, "")
+    .split(" ")
+    .map((x) => x.trim())
+    .filter((x) => x.length > 0);
+  const where = texts.map((x) => like(food.description, `%${x}%`));
   const results = await db
     .select({
       id: food.fdcId,
       description: food.description,
     })
     .from(food)
-    .where(like(food.description, `%${text}%`))
+    .where(and(...where))
     .limit(20)
     .orderBy(sql`difference(${food.description}, ${text})`)
     .all();
 
   return c.json(results);
 });
+
+// app.get("/food/:id", async (c) => {
+//   const text = c.req.param("id");
+//   const results = await db
+//     .select({
+//       id: food.fdcId,
+//       description: food.description,
+//       nutrients: { foo: "bar" },
+//     })
+//     .from(food)
+//     .where(like(food.description, `%${text}%`))
+//     .limit(20)
+//     .orderBy(sql`difference(${food.description}, ${text})`)
+//     .all();
+
+//   return c.json(results);
+// });
 
 serve(app);
